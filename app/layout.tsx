31be158fe0704d3ac3842fba54599919ca24b67b
Soumyadip Maity <maitysoumyadip108@gmail.com>
2026-02-19 10:58:27 +0530

    added the icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,10 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
 };
 
 export default function RootLayout({
